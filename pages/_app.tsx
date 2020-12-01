efa756cd4a17b5744df6df872067ef262dcaa76d
Rafael Gomes Cavalcanti <rafael@homehub.com.br>
2020-12-01 19:58:13 -0300

    fix: ajusta bug de preview de compartilhamento

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
           content="Acesse agora o maior e-commecerce para compra e venda de pokemón!"
         />
         <meta
-          property="og:image"
+          property="og:image:secure"
           content="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
         />
 
